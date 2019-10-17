1e57930933919d2b10870058605ea6ef33ecf347
Patrick Murphy <pmurp104@ford.com>
2019-10-16 22:47:48 -0400

    Got addTodo working

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -14,12 +14,19 @@ const actions = {
         const response = await axios.get('http://jsonplaceholder.typicode.com/todos');
 
         commit('setTodos', response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('http://jsonplaceholder.typicode.com/todos', 
+            {title, completed: false});
+
+        commit('newTodo', response.data);
     }
 };
 
 // adds response to the state
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos)
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo)
 };
 
 export default {
